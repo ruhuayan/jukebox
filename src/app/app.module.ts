2354491cf5aa38634c5f2d47204e97f3029f1e68
Richard Yan <Ruhua.Yan@mindgeek.com>
2019-10-11 17:07:37 -0400

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { FooterComponent } from './footer/footer.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChatbotModule } from './chatbot/chatbox.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -17,7 +19,8 @@ import { ChatbotModule } from './chatbot/chatbox.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ChatbotModule
+    ChatbotModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
