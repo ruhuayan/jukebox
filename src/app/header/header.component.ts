9abd676cd9d47a2b87cd9c8b9c58b922261040a5
ruhuayan <ruhuayan@gmail.com>
2019-10-13 21:17:51 -0400

    mobile menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('inputToggle') inputToggle: ElementRef<HTMLElement>;
+  linkUrl = 'igame';
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  clickLink(url: string): void {
+    this.inputToggle.nativeElement.click();
+    this.linkUrl = url;
+    setTimeout(() => this.router.navigateByUrl(url), 480);
+  }
 }
