eb7258aba884ef3f76c95bf4dd5c88a53f334413
Richard Yan <Ruhua.Yan@mindgeek.com>
2019-10-21 17:38:50 -0400

    faceapi component

--- src/app/pages/faceapi/faceapi.component.ts
+++ src/app/pages/faceapi/faceapi.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy, ElementRef, Renderer2 } from '@angular/core';
 // import { Observable } from 'rxjs/internal/Observable';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { from } from 'rxjs/internal/observable/from';
 import * as faceapi from 'face-api.js';
+import { forkJoin, interval } from 'rxjs';
 
 @Component({
   selector: 'app-faceapi',
@@ -10,12 +11,15 @@ import * as faceapi from 'face-api.js';
   styleUrls: ['./faceapi.component.scss']
 })
 export class FaceapiComponent implements OnInit, OnDestroy {
-  @ViewChild('videoEl') videoEl: any;
+  @ViewChild('videoEl') videoEl: ElementRef;
+  @ViewChild('canvas') canvas: ElementRef;
   private _navigator = <any> navigator;
   private subscription: Subscription;
+  private intervalSubscribe: Subscription;
   private video: any;
+  public supportMedia = true;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
     this.video = this.videoEl.nativeElement;
@@ -23,68 +27,65 @@ export class FaceapiComponent implements OnInit, OnDestroy {
     this._navigator.getUserMedia = ( this._navigator.getUserMedia || this._navigator.webkitGetUserMedia
       || this._navigator.mozGetUserMedia || this._navigator.msGetUserMedia );
 
-    // Promise.all([
-    //   faceapi.nets.tinyFaceDetector.loadFromUri('/assets/models'),
-    //   faceapi.nets.faceLandmark68Net.loadFromUri('/assets/models'),
-    //   faceapi.nets.faceRecognitionNet.loadFromUri('/assets/models'),
-    //   faceapi.nets.faceExpressionNet.loadFromUri('/assets/models')
-    // ]).then(this.startVideo);
-    this.startVideo();
+    const promises = [faceapi.nets.tinyFaceDetector.loadFromUri('/assets/models'),
+                      faceapi.nets.faceLandmark68Net.loadFromUri('/assets/models'),
+                      faceapi.nets.faceRecognitionNet.loadFromUri('/assets/models'),
+                      faceapi.nets.faceExpressionNet.loadFromUri('/assets/models')
+                    ];
+
+    const sub = forkJoin(promises).subscribe(() => {
+      this.startVideo();
+      sub.unsubscribe();
+    });
+    
   }
 
   private startVideo(): void {
 
-    const promise = this._navigator.mediaDevices.getUserMedia({video: true, audio: true});
+    const promise = this._navigator.mediaDevices.getUserMedia({video: true, audio: false});
     this.subscription = from(promise).subscribe(
                           stream => {
                             this.video.srcObject  = stream;
                           },
                           err => {
+                            this.supportMedia = false;
                             console.log(err);
                           }
                         );
-
-    // this.video.addEventListener('play', () => { console.log('play')
-    //   const canvas = faceapi.createCanvasFromMedia(this.video);
-    //   document.body.append(canvas);
-    //   const displaySize = { width: this.video.width, height: this.video.height };
-    //   faceapi.matchDimensions(canvas, displaySize);
-    //   setInterval(async () => {
-    //     const detections = await faceapi.detectAllFaces(this.video, new faceapi.TinyFaceDetectorOptions())
-    //                                     .withFaceLandmarks().withFaceExpressions();
-    //     const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    //     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-    //     faceapi.draw.drawDetections(canvas, resizedDetections);
-    //     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-    //     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-    //   }, 100);
-    // });
   }
 
   public start(): void {
   }
 
-  ngOnDestroy() {
-    if (this.subscription) { this.subscription.unsubscribe(); }
+  public detect(): void {
+    console.log('loaded');
+    const canvas = this.canvas.nativeElement; //faceapi.createCanvasFromMedia(this.video);
+
+    const displaySize = { width: this.video.offsetWidth, height: this.video.offsetHeight };
+    faceapi.matchDimensions(canvas, displaySize);
+
+    const source = interval(100);
+    this.intervalSubscribe = source.subscribe(async () => {
+        const detections = await faceapi.detectAllFaces(this.video, new faceapi.TinyFaceDetectorOptions())
+                                        .withFaceLandmarks().withFaceExpressions(); console.log(detections)
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    });
   }
-  /****
-   interface HTMLCanvasElement extends HTMLElement {
-      // ...
-      transferControlToOffscreen(): OffscreenCanvas;
-    }
 
-    interface OffscreenCanvasRenderingContext2D extends CanvasState, CanvasTransform, CanvasCompositing, CanvasImageSmoothing, CanvasFillStrokeStyles, CanvasShadowStyles, CanvasFilters, CanvasRect, CanvasDrawPath, CanvasUserInterface, CanvasText, CanvasDrawImage, CanvasImageData, CanvasPathDrawingStyles, CanvasTextDrawingStyles, CanvasPath {
-      readonly canvas: OffscreenCanvas;
+  public end():void {
+    console.log('ended');
+    this.intervalSubscribe.unsubscribe();
+  }
+  ngOnDestroy() {
+    if (this.subscription) { 
+      this.subscription.unsubscribe();
     }
-    // declare var OffscreenCanvasRenderingContext2D: {
-    //   prototype: OffscreenCanvasRenderingContext2D;
-    //   new(): OffscreenCanvasRenderingContext2D;
-    // }
-    interface OffscreenCanvas extends EventTarget {
-      width: number;
-      height: number;
-      getContext(contextId: "2d", contextAttributes?: CanvasRenderingContext2DSettings): OffscreenCanvasRenderingContext2D | null;
+    if (this.intervalSubscribe) {
+        this.intervalSubscribe.unsubscribe();
     }
-
-   */
+  }
 }
